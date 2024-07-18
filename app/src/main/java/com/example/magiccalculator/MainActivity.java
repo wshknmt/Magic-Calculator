218d1f3f9cb53580f0f3a0ded9bd7752205ac8a3
Pawel Martyniuk <p.martyniuk21@gmail.com>
2024-07-18 08:47:18 +0200

    added floating point numbers

--- app/src/main/java/com/example/magiccalculator/MainActivity.java
+++ app/src/main/java/com/example/magiccalculator/MainActivity.java
@@ -18,14 +18,25 @@ import androidx.core.content.ContextCompat;
 
 import com.example.magiccalculator.databinding.ActivityMainBinding;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private static final int PICK_CONTACT = 1;
     private String selectedNumber;
-    private char lastCharacter = 'E'; // N - number, O - operator, E - empty
+    private char lastCharacter = 'E'; // N - number, O - operator, E - empty, C - comma
     private Boolean pickedNumber = false;
-
+    private double firstNumber = 0, secondNumber = 0;
+    private Boolean existSecondNumber = false;
+    private char operator;
+    private Boolean pickedOperator = false;
+    private Boolean commaExistFirst = false;
+    private Boolean commaExistSecond = false;
+    private int exponentFirst = 1;
+    private int exponentSecond = 1;
+    double precision = Math.pow(10, 6);
+//    DecimalFormat df = new DecimalFormat("#.######");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,14 +59,41 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonMinus.setOnClickListener(v -> operatorClicked('-'));
         binding.buttonMultiply.setOnClickListener(v -> operatorClicked('*'));
         binding.buttonDivide.setOnClickListener(v -> operatorClicked('/'));
-//        binding.buttonPercent.setOnClickListener(v -> operatorClicked('%'));
-//        binding.buttonComma.setOnClickListener(v -> operatorClicked(','));
+        binding.buttonPercent.setOnClickListener(v -> operatorClicked('%'));
+
+        binding.buttonComma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lastCharacter == 'N') {
+                    if (!existSecondNumber && !commaExistFirst) {
+                        commaExistFirst = true;
+                    } else if (existSecondNumber && !commaExistSecond) {
+                        commaExistSecond = true;
+                    } else return;
+                    String currentText = binding.resultText.getText().toString();
+                    String newText = getString(R.string.result_text_placeholder, currentText + '.');
+                    binding.resultText.setText(newText);
+                    lastCharacter = 'C';
+//                    commaExist = true;
+                }
+            }
+        });
 
         binding.buttonAllClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 binding.resultText.setText("0");
                 lastCharacter = 'E';
+                pickedOperator = false;
+                firstNumber = 0;
+                secondNumber = 0;
+                existSecondNumber = false;
+                operator = ' ';
+                pickedNumber = false;
+                commaExistFirst = false;
+                commaExistSecond = false;
+                exponentFirst = 1;
+                exponentSecond = 1;
             }
         });
 
@@ -65,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 if (lastCharacter != 'E' && binding.resultText.length() > 0) {
                     String text = binding.resultText.getText().toString();
                     binding.resultText.setText(text.substring(0, text.length() - 1));
+//                    if ()
                 }
                 if (binding.resultText.length() == 0) {
                     lastCharacter = 'E';
@@ -77,12 +116,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (pickedNumber) {
-                    String text = binding.resultText.getText().toString();
+//                    String text = binding.resultText.getText().toString();
                     binding.resultText.setText(selectedNumber);
                 } else {
-
+                    if (existSecondNumber) {
+                        double countedResult = makeCalculations();
+                        countedResult = Math.floor(countedResult * precision) / precision;
+                        String strResult = String.valueOf(countedResult);
+                        if (Math.floor(countedResult) == countedResult) {
+                            strResult = strResult.split("\\.")[0];
+                            commaExistFirst = false;
+                            exponentFirst = 1;
+                        } else {
+                            String[] fractionalParts = strResult.split("\\.");
+                            if (fractionalParts.length < 2) {
+                                commaExistFirst = false;
+                                exponentFirst = 1;
+                            } else {
+                                exponentFirst = fractionalParts[1].length();
+                            }
+                        }
+                        binding.resultText.setText(strResult);
+                        firstNumber = countedResult;
+                        secondNumber = 0;
+                        existSecondNumber = false;
+                        commaExistSecond = false;
+                        exponentSecond = 1;
+                        pickedOperator = false;
+                        lastCharacter = 'N';
+                    }
                 }
-
             }
         });
 
@@ -133,18 +196,89 @@ public class MainActivity extends AppCompatActivity {
         String newText = getString(R.string.result_text_placeholder, currentText + character);
         binding.resultText.setText(newText);
         lastCharacter = 'N';
+        if (!pickedOperator) {
+            if (!commaExistFirst)
+                firstNumber = firstNumber * 10 + character - '0';
+            else {
+                double quotient = Math.pow(10, exponentFirst);
+                firstNumber = firstNumber + (character - '0') / quotient;
+                exponentFirst++;
+            }
+        } else {
+            existSecondNumber = true;
+            if (!commaExistSecond)
+                secondNumber = secondNumber * 10 + character - '0';
+            else {
+                double quotient = Math.pow(10, exponentSecond);
+                secondNumber = secondNumber + (character - '0') / quotient;
+                exponentSecond++;
+            }
+        }
     }
 
     void operatorClicked(char character) {
         if (lastCharacter == 'N') {
-            String currentText = binding.resultText.getText().toString();
-            String newText = getString(R.string.result_text_placeholder, currentText + character);
-            binding.resultText.setText(newText);
+            if (!existSecondNumber) {
+                String currentText = binding.resultText.getText().toString();
+                String newText = getString(R.string.result_text_placeholder, currentText + character);
+                binding.resultText.setText(newText);
+            } else {
+                double countedResult = makeCalculations();
+                countedResult = Math.floor(countedResult * precision) / precision;
+                String strResult = String.valueOf(countedResult);
+                if (Math.floor(countedResult) == countedResult) {
+                    strResult = strResult.split("\\.")[0];
+                    commaExistFirst = false;
+                    exponentFirst = 1;
+                } else {
+                    String[] fractionalParts = strResult.split("\\.");
+                    if (fractionalParts.length < 2) {
+                        commaExistFirst = false;
+                        exponentFirst = 1;
+                    } else {
+                        exponentFirst = fractionalParts[1].length();
+                    }
+                }
+
+                strResult += character;
+                binding.resultText.setText(strResult);
+                firstNumber = countedResult;
+                secondNumber = 0;
+                existSecondNumber = false;
+                commaExistSecond = false;
+                exponentSecond = 1;
+
+            }
+
         } else if (lastCharacter == 'O') {
             String currentText = binding.resultText.getText().toString();
             String newText = currentText.substring(0, currentText.length() - 1) + character;
             binding.resultText.setText(newText);
-        }
+        } else return;
         lastCharacter = 'O';
+        pickedOperator = true;
+        operator = character;
+    }
+
+    double makeCalculations() {
+        double result = 0.0;
+        switch(operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+            case '%':
+                result = firstNumber * 100 / secondNumber;
+                break;
+        };
+        return result;
     }
 }
